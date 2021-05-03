ef101c7c1471e5d12a5557092b1b7c9364dbfb96
Nivrette <72909694+Nivrette@users.noreply.github.com>
2021-05-03 17:12:08 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,30 +1,30 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
+let myNodelist = document.getElementsByTagName("LI");
+let i;
 for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
 // Click on a close button to hide the current task
-var close = document.getElementsByClassName("close");
-var i;
+let close = document.getElementsByClassName("close");
+let i;
 for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    var div = this.parentElement;
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }
 
 // Create a new task when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var dateValue = document.getElementById("myDate").value;
-  var t = document.createTextNode("Date limite : " + dateValue + " - Tâche à effectuer : " + inputValue);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let dateValue = document.getElementById("myDate").value;
+  let t = document.createTextNode("Date limite : " + dateValue + " - Tâche à effectuer : " + inputValue);
   li.appendChild(t);
   if (inputValue === '') {
     alert("You must write something!");
@@ -36,15 +36,15 @@ function newElement() {
   document.getElementById("myInput").value = "";
   document.getElementById("myDate").value = "";
   
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
